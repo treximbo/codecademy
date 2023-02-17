2e629be7c561cf5e68e2cb6f9b67e6494e0fd680
treximbo <Treximbo@gmail.com>
2023-02-16 21:27:16 -0500

    InteliJ initital commmit

---
+++ java/HelloWorld.java
@@ -0,0 +1,9 @@
+// class - must match the name of the file in 'pascal' case
+public class HelloWorld {
+    // method called 'main()' - a sequence of tasks for the comp to execute
+    // holds all the instructions for the program
+    public static void main(String[] args) {
+        // statement
+        System.out.println("Hello, World!");
+    }
+}
